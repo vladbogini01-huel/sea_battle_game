33467d72b628be462944b74412b3f10ffcb6b90c
Z1gSt1K <mp910265@gmail.com>
2026-03-12 23:50:55 +0300

    one more change

---
+++ build/_deps/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yaga/sea_battle_game/build/_deps/googletest-src/googletest
+# Build directory: /home/yaga/sea_battle_game/build/_deps/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
