885f7cbd4d15d2b119623158a158c2aac8427183
Z1gSt1K <mp910265@gmail.com>
2026-03-13 00:01:18 +0300

    Oh no...

---
+++ build/CMakeFiles/sea_battle_tests.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/yaga/sea_battle_game/tests/test_board.cpp" "CMakeFiles/sea_battle_tests.dir/tests/test_board.cpp.o" "gcc" "CMakeFiles/sea_battle_tests.dir/tests/test_board.cpp.o.d"
+  "/home/yaga/sea_battle_game/tests/test_fleet.cpp" "CMakeFiles/sea_battle_tests.dir/tests/test_fleet.cpp.o" "gcc" "CMakeFiles/sea_battle_tests.dir/tests/test_fleet.cpp.o.d"
+  "/home/yaga/sea_battle_game/tests/test_game.cpp" "CMakeFiles/sea_battle_tests.dir/tests/test_game.cpp.o" "gcc" "CMakeFiles/sea_battle_tests.dir/tests/test_game.cpp.o.d"
+  "/home/yaga/sea_battle_game/tests/test_main.cpp" "CMakeFiles/sea_battle_tests.dir/tests/test_main.cpp.o" "gcc" "CMakeFiles/sea_battle_tests.dir/tests/test_main.cpp.o.d"
+  "/home/yaga/sea_battle_game/tests/test_player.cpp" "CMakeFiles/sea_battle_tests.dir/tests/test_player.cpp.o" "gcc" "CMakeFiles/sea_battle_tests.dir/tests/test_player.cpp.o.d"
+  "/home/yaga/sea_battle_game/tests/test_ships.cpp" "CMakeFiles/sea_battle_tests.dir/tests/test_ships.cpp.o" "gcc" "CMakeFiles/sea_battle_tests.dir/tests/test_ships.cpp.o.d"
+  "" "sea_battle_tests" "gcc" "CMakeFiles/sea_battle_tests.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/sea_battle_tests.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for sea_battle_tests.
+# This may be replaced when dependencies are built.
